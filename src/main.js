da4fd6877b59f9902d935912cdc6b21f9529524f
ChawitSupcharoen <123621026+ChawitSupcharoen@users.noreply.github.com>
2025-05-04 16:10:13 +0700

    Link spline with native javascript

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-
-import { Application } from '@splinetool/runtime';
+import { Application } from "@splinetool/runtime";
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/9Q5dBRAbLKVzmrFy/scene.splinecode');
+app.load('https://prod.spline.design/0rJSSVnkvWOOwtLX/scene.splinecode').then(() => {
+  app.addEventListener('mouseDown', (e) => {
+
+      if (e.target.name === "Test Cube"){
+        console.log("Magic cube got click");
+      }
+      
+    
+  });
+});
